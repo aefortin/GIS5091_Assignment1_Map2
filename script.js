a00c3f8d5b3f33dcbe88f49d3eb861a0375b06ee
aefortin <154523554+aefortin@users.noreply.github.com>
2024-01-24 17:02:51 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+var map = L.map('map').setView([37.7, -122.4], 10);
+
+
+L.tileLayer('https://a.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: 'Map tiles by Stamen Design, under CC BY 3.0.',
+        maxZoom: 18
+}).addTo(map);
+
+  $.getJSON("https://raw.githubusercontent.com/gbrunner/adv-programming-for-gis-and-rs/master/Web%20Development%20Module/Unit%201%20-%20GitHub%20and%20Leaflet/sf_crime.geojson",function(data){
+
+    
+    var ratIcon = L.icon({
+    iconUrl: 'https://maptimeboston.github.io/leaflet-intro/rat.png',
+    iconSize: [20,20]
+  }); 
+  L.geoJson(data  ,{
+    pointToLayer: function(feature,latlng){
+	  return L.marker(latlng,{icon: ratIcon});
+    }
+  } ).addTo(map);
+  });
